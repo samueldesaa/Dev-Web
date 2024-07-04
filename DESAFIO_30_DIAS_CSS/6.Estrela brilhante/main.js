7d5e994f86e3c004f63369c9af4aebe7aa647cba
Samuel de Sá <76416261+samueldesaa@users.noreply.github.com>
2024-07-04 15:19:05 -0300

    Atualização projeto estrela

--- DESAFIO_30_DIAS_CSS/6.Estrela brilhante/main.js
+++ DESAFIO_30_DIAS_CSS/6.Estrela brilhante/main.js
@@ -1,21 +1,20 @@
 document.addEventListener("mousemove", parallax);
+document.addEventListener("mousemove", color);
 
 function parallax(e) {
     this.querySelectorAll(".estrela").forEach((star) => {
         var p = document.getElementById("texto");
         const speed = star.getAttribute("data-speed");
         let x,y;
-        if (e.pageX > (window.innerWidth / 2)) {
-            x = e.pageX % (window.innerWidth / 2);
-        } else {
-            x = e.pageX + (window.innerWidth / 2);
-        }
-        if (e.pageY > (window.innerHeight / 2)) {
-            y = e.pageY % (window.innerHeight / 2);
-        } else {
-            y = e.pageY + (window.innerHeight / 2);
-        }
-        p.innerText = "Width: " + window.innerWidth + ", " + "Height: " + window.innerHeight + "\n" + "mX: " + e.pageX + ", mY: " + e.pageY + "\nx: " + x + ", y:" + y;
+        x = ((window.innerWidth/2)-e.pageX)/10*speed;
+        y = ((window.innerHeight/2)-e.pageY)/10*speed;
+        p.innerText = `Width: ${window.innerWidth}, Height: ${window.innerHeight}\nmX: ${e.pageX}, mY: ${e.pageY}`;
         star.style.transform = `translateX(${x}px) translateY(${y}px)`;
     });
+}
+
+function color(e){
+    if(e.pageX>(window.innerWidth/0.7)){
+        document.body.get = `rgb(${parseInt(Math.ramdom()*256)}, ${parseInt(Math.ramdom()*256)}, ${parseInt(Math.ramdom()*256)})`;
+    }
 }
