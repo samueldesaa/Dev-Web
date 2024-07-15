0b81a83df60fd6b64230e9fdece6de480ec6b33a
Samuel de Sá <samueldsa03032005@gmail.com>
2024-07-15 10:29:54 -0300

    Atualização ALURA

---
+++ PROJETOS CURSO ALURA/js-curso-2-aula_3/app.js
@@ -0,0 +1,69 @@
+let listaSort = [];
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+let numMax = 4;
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', `Escolha um número entre 1 e ${numMax}`);
+}
+
+exibirMensagemInicial();
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
+
+function gerarNumeroAleatorio() {
+    let numeAleatorio = parseInt(Math.random() * numMax + 1);
+    if(listaSort.length == numMax){
+        listaSort = [];
+    }
+    if(listaSort.includes(numeAleatorio)){
+        return gerarNumeroAleatorio();
+    }else{
+        listaSort.push(numeAleatorio);
+        return numeAleatorio;
+    }
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true)
+}
+
+
+
+
+
+
+
