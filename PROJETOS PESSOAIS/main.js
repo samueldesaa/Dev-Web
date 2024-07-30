513c4196b8737c9e0c9b95bfd467218d7a3cd935
Samuel de Sá <samueldsa03032005@gmail.com>
2024-07-30 11:35:54 -0300

    Atualização Projetos Pessoais

---
+++ PROJETOS PESSOAIS/main.js
@@ -0,0 +1,37 @@
+const dataEspecifica = new Date("2023-07-29");
+const dataAtual = new Date();
+const diferencaEmMilissegundos = dataAtual - dataEspecifica;
+const milissegundosPorDia = 24 * 60 * 60 * 1000;
+const diferencaEmDias = Math.floor(diferencaEmMilissegundos / milissegundosPorDia);
+
+let agua = diferencaEmDias%2 == 0 ? "SIM" : "NÃO";
+let aguaP = document.getElementById('agua');
+aguaP.innerText = agua;
+if(agua){
+    aguaP.style.backgroundImage = "linear-gradient(to top, transparent, red)";
+}else{
+    aguaP.style.backgroundImage = "linear-gradient(bottom, transparent, #57b8a1)";
+}
+aguaP.style.backgroundClip = "text"
+aguaP.style.color = "transparent"
+
+function diaDeLixo(data) {
+    const diaDaSemana = data.getDay();
+    return diaDaSemana === 1 || diaDaSemana === 3 || diaDaSemana === 5;
+}
+
+const dataParaVerificar = new Date();
+
+const lixo = diaDeLixo(dataParaVerificar) ? "SIM" : "NÃO";
+
+let lixoP = document.getElementById('lixo');
+
+lixoP.innerText = lixo;
+
+if(lixo){
+    lixoP.style.backgroundImage = "linear-gradient(to top, transparent, red)";
+}else{
+    lixoP.style.backgroundImage = "linear-gradient(bottom, transparent, #57b8a1)";
+}
+lixoP.style.backgroundClip = "text"
+lixoP.style.color = "transparent"
