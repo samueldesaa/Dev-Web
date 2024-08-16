ffa36799c325a28cf23b1a86d9afb64f7b90a079
Samuel de Sá <samueldsa03032005@gmail.com>
2024-08-16 17:21:39 -0300

    Update main.js

--- PROJETOS PESSOAIS/Dias de Tarefas/main.js
+++ PROJETOS PESSOAIS/Dias de Tarefas/main.js
@@ -1,7 +1,13 @@
 const dataEspecifica = new Date("2024-08-02");
 const dataAtual = new Date();
+
+// Ajuste para considerar a hora do dia
+dataEspecifica.setHours(0, 1, 0, 0);
+dataAtual.setHours(0, 0, 0, 0);
+
 console.log(dataEspecifica);
 console.log(dataAtual);
+
 const diferencaEmMilissegundos = dataAtual - dataEspecifica;
 const milissegundosPorDia = 24 * 60 * 60 * 1000;
 const diferencaEmDias = Math.floor(diferencaEmMilissegundos / milissegundosPorDia);
@@ -26,6 +32,7 @@ function diaDeLixo(data) {
     return diaDaSemana === 1 || diaDaSemana === 3 || diaDaSemana === 5;
 }
 
+// Ajuste para considerar a hora atual
 const dataParaVerificar = new Date();
 const lixo = diaDeLixo(dataParaVerificar) ? "SIM" : "NÃO";
 
@@ -36,8 +43,8 @@ if (lixo !== "SIM") {
     lixoP.style.backgroundImage = "linear-gradient(to top, transparent, red)";
     lixoP.style.backgroundImage = "-webkit-linear-gradient(to top, transparent, red)";
 } else {
-    lixoP.style.backgroundImage = "linear-gradient(bottom, transparent, green)";
-    lixoP.style.backgroundImage = "-webkit-linear-gradient(bottom, transparent, green)";
+    lixoP.style.backgroundImage = "linear-gradient(to top, transparent, green)";
+    lixoP.style.backgroundImage = "-webkit-linear-gradient(to top, transparent, green)";
 }
 lixoP.style.backgroundClip = "text";
 lixoP.style.color = "transparent";
