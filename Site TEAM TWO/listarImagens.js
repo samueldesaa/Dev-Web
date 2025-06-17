5454fe049556b988cd9548b736c6bf89c9b0b144
Samuel de Sá <samueldsa03032005@gmail.com>
2025-06-16 22:19:42 -0300

    Adicionando brindes de valores diferentes

---
+++ Site TEAM TWO/listarImagens.js
@@ -0,0 +1,25 @@
+const items5 = [
+  { nome: "Espada Ouro", imagem: "Itens/Espada Ouro.png" },
+  { nome: "Machado Ouro", imagem: "Itens/Machado Ouro.png" },
+  { nome: "Picareta Ouro", imagem: "Itens/Picareta Ouro.png" },
+  { nome: "Raposa Laranja", imagem: "Itens/Raposa Laranja.png" },
+  { nome: "Raposa Rubi", imagem: "Itens/Raposa Rubi.png" },
+  { nome: "Raposa Branca", imagem: "Itens/Raposa Branca.png" },
+  { nome: "Yoshi Azul", imagem: "Itens/Yoshi Azul.png" },
+  { nome: "Yoshi Preto", imagem: "Itens/Yoshi Preto.png" },
+  { nome: "Yoshi Amarelo", imagem: "Itens/Yoshi Amarelo.png" },
+  { nome: "Yoshi Verde", imagem: "Itens/Yoshi Verde.png" },
+  { nome: "Pa Ouro", imagem: "Itens/Pa Ouro.png" }
+];
+
+const items10 = [
+  { nome: "Yoshi Preto", imagem: "Itens/Yoshi Preto.png" },
+  { nome: "Yoshi Preto", imagem: "Itens/Yoshi Preto.png" },
+  { nome: "Yoshi Preto", imagem: "Itens/Yoshi Preto.png" },
+  { nome: "Yoshi Preto", imagem: "Itens/Yoshi Preto.png" },
+  { nome: "Yoshi Preto", imagem: "Itens/Yoshi Preto.png" },
+  { nome: "Yoshi Preto", imagem: "Itens/Yoshi Preto.png" },
+  { nome: "Yoshi Preto", imagem: "Itens/Yoshi Preto.png" },
+  { nome: "Yoshi Verde", imagem: "Itens/Yoshi Verde.png" },
+  { nome: "Pa Ouro", imagem: "Itens/Pa Ouro.png" }
+];
